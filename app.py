2139791db18317b2c8f34712272d1963bf2f1ec9
Mayur Pujari <mayur.pujari@cloudstrats.com>
2024-12-05 09:19:29 +0530

    Deloyed link

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def get_db_connection():
 
         # Connect to the database
         conn = psycopg2.connect(
-            database=result.path[1:],  # Remove the leading '/' from the database name
+            database=result.path[1:],  
             user=result.username,
             password=result.password,
             host=result.hostname,
